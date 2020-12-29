ab5fe394f8bddd4f8ee2a54f3a89043e3c8566ef
Alexander Willner <alex@willner.ws>
2020-12-29 20:13:53 +0100

    styling

--- src/mailbox_message.py
+++ src/mailbox_message.py
@@ -134,7 +134,8 @@ Tool: https://mailboxcleanup.netcee.de
                 if iterator == 0:
                     logging.debug(
                         '      Conflict\t: Resolving same file / other hash..')
-                target = self._copy_file(source, target_name, date, iterator + 1)
+                target = self._copy_file(source, target_name, date,
+                                         iterator + 1)
             else:
                 logging.debug('      Moving\t: Already exists (same hash)')
 
